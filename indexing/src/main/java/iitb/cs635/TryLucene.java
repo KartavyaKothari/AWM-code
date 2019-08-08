97669585eaa79afd7fe96a3fa9a7fc7066869527
Soumen Chakrabarti <soumen.chakrabarti@gmail.com>
2019-08-08 10:32:55 +0530

    Fixes Java tab formatting.

--- indexing/src/main/java/iitb/cs635/TryLucene.java
+++ indexing/src/main/java/iitb/cs635/TryLucene.java
@@ -20,46 +20,44 @@ import org.apache.lucene.store.RAMDirectory;
 
 import java.io.IOException;
 
-	public class TryLucene {
-	public static void main(String[] args) 
+public class TryLucene {
+    public static void main(String[] args)
                 throws IOException, ParseException
-        {
-	    StandardAnalyzer analyzer = new StandardAnalyzer();
-	    Directory index = new RAMDirectory();
-	
-	    IndexWriterConfig config = new IndexWriterConfig(analyzer);
-	
-	    IndexWriter w = new IndexWriter(index, config);
-	    addDoc(w, "Lucene in Action", "1");
-	    addDoc(w, "Lucene for Dummies", "2");
-	    addDoc(w, "Java", "3");
-	    addDoc(w, "Oracle", "4");
-	    w.close();
-	
-	    String querystr = "java";
-	
-	    Query q = new QueryParser("title", analyzer).parse(querystr);
-	
-	    int hitsPerPage = 10;
-	    IndexReader reader = DirectoryReader.open(index);
-	    IndexSearcher searcher = new IndexSearcher(reader);
-	    TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-	    searcher.search(q, collector);
-	    ScoreDoc[] hits = collector.topDocs().scoreDocs;
-	    
-	    System.out.println("Found " + hits.length + " hits.");
-	    for(int i=0;i<hits.length;++i) {
-	      int docId = hits[i].doc;
-	      Document d = searcher.doc(docId);
-	      System.out.println("docid=" + docId + " isbn=" + d.get("isbn") + " title=" + d.get("title"));
-	    }
-	    reader.close();
-	  }
-	
-	  private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-	    Document doc = new Document();
-	    doc.add(new TextField("title", title, Field.Store.YES));
-	    doc.add(new StringField("isbn", isbn, Field.Store.YES));
-	    w.addDocument(doc);
-	  }
+    {
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Directory index = new RAMDirectory();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter w = new IndexWriter(index, config);
+        addDoc(w, "Lucene in Action", "1");
+        addDoc(w, "Lucene for Dummies", "2");
+        addDoc(w, "Java", "3");
+        addDoc(w, "Oracle", "4");
+        w.close();
+        System.out.println("finished indexing");
+
+        String querystr = "oRaClE of delpHi";
+        Query q = new QueryParser("title", analyzer).parse(querystr);
+        int hitsPerPage = 10;
+        IndexReader reader = DirectoryReader.open(index);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
+        searcher.search(q, collector);
+        ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+        System.out.println("Found " + hits.length + " hits.");
+        for(int i=0;i<hits.length;++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println("docid=" + docId + " isbn=" + d.get("isbn") + " title=" + d.get("title"));
+        }
+        reader.close();
+    }
+
+    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        w.addDocument(doc);
+    }
 }
+
